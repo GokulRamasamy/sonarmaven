d6e78aa354b4853cfed74023448b6033d3314762
gokulkannan <gokulkannan.r@raster.in>
2018-03-14 11:39:40 +0530

    sonar maven

---
+++ src/main/java/com/sonar/sonarmaven/SonarmavenApplication.java
@@ -0,0 +1,12 @@
+package com.sonar.sonarmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarmavenApplication.class, args);
+	}
+}
